3b080d26f9df7ccf2eaa6d48b86a89c63e72cdfd
Agnes-Ruescas <agnes.ruescas@outlook.com>
2020-10-01 17:34:51 -0300

    Primeira versão

--- ZUZ-FrontEnd/zuz/src/app/app.module.ts
+++ ZUZ-FrontEnd/zuz/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
