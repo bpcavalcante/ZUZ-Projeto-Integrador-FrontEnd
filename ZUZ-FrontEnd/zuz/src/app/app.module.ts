9e63ee0b37b848f2c1c1afe842ee121fcc2487e8
Fabio Casaroti <fabiocasaroti@hotmail.com>
2020-10-01 17:50:40 -0300

    Footer e Perfil Lateral

--- ZUZ-FrontEnd/zuz/src/app/app.module.ts
+++ ZUZ-FrontEnd/zuz/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PerfilLateralComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
