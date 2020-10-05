0a19b647627f03f79d8d6d06d5b0b4081063bc25
Alex Cesar <alexesar.n@gmail.com>
2020-10-05 09:43:31 -0300

    teste merge

--- ZUZ-FrontEnd/zuz/src/app/app.module.ts
+++ ZUZ-FrontEnd/zuz/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+<<<<<<< HEAD
 import { NavbarComponent } from './navbar/navbar.component';
+=======
+import { HomeComponent } from './home/home.component';
+>>>>>>> f281a11e544d34002224f0491dfd92367d66f046
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     NavbarComponent
+=======
+    HomeComponent
+>>>>>>> f281a11e544d34002224f0491dfd92367d66f046
   ],
   imports: [
     BrowserModule,
